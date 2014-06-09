c8cffb7f76acfa8bb4a9037e386717037933205d
Kevin Vess <kevinvess@gmail.com>
2014-06-09 18:15:00 -0400

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== Force Login ===
+Contributors: kevinvess
+Tags: force user login, login, private, privacy, protected
+Requires at least: 2.0
+Tested up to: 3.9.1
+Stable tag: trunk
+License: GPLv2 or later
+
+Force Login is a simple lightweight plugin that requires visitors to log in to interact with the website.
+
+== Description ==
+
+Easily hide your WordPress site from public viewing by requiring visitors to log in first. As simple as flipping a switch.
+
+Make your website private until it's ready to share publicly, or keep it private for members only.
+
+== Installation ==
+
+Upload the Force Login plugin to your site, then Activate it.
+
+1, 2: You're done!
---
+++ wp-force-login.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: Force Login
+Plugin URI: http://vess.me/
+Description: Easily hide your WordPress site from public viewing by requiring visitors to log in first. Activate to turn on.
+Version: 1.0
+Author: Kevin Vess
+Author URI: http://vess.me/
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+function getUrl() {
+  $url  = @( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] : 'https://'.$_SERVER["SERVER_NAME"];
+  $url .= $_SERVER["REQUEST_URI"];
+  return $url;
+}
+function lockdown() {
+  if( !is_user_logged_in() && getUrl() != wp_login_url() ) {
+    wp_redirect( wp_login_url(), 302 );
+    exit();
+  }
+}
+add_action('init', 'lockdown');
