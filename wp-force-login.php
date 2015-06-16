47fd3511764dd2b61855cd3d6127705e38694ee1
Joachim Happel <joachim.happel@gmail.com>
2015-06-16 18:13:07 +0200

    filter added: v_forcelogin_stop_handle
    /**
    * Description: this filter ist more flexible then the whitelist filter 
    *
    * example usage: the requested Url you wan't redirect starts with: "http://example.org/my-special-controller/?ticket=" 
    * and may be has same more params
    *
    * the custom filter returns true (default is false) to stop redericting to the wp-login.
    *
    */
    
    function my_forcelogin_check_request($bool, $url){
    	
    	if (strpos($url,'/my-special-controller/?ticket=') > 0  ){
    		$bool = true;
    	}
    	return $bool;
    }
    add_filter('v_forcelogin_stop_handle', 'my_forcelogin_check_request', 10,2);

--- wp-force-login.php
+++ wp-force-login.php
@@ -25,20 +25,22 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-function v_getUrl() {
-  $url  = isset( $_SERVER['HTTPS'] ) && 'on' === $_SERVER['HTTPS'] ? 'https' : 'http';
-  $url .= '://' . $_SERVER['SERVER_NAME'];
-  $url .= in_array( $_SERVER['SERVER_PORT'], array('80', '443') ) ? '' : ':' . $_SERVER['SERVER_PORT'];
-  $url .= $_SERVER['REQUEST_URI'];
-  return $url;
-}
 function v_forcelogin() {
   if( !is_user_logged_in() ) {
-    $url = v_getUrl();
-    $whitelist = apply_filters('v_forcelogin_whitelist', array());
+	
+	$url  = isset( $_SERVER['HTTPS'] ) && 'on' === $_SERVER['HTTPS'] ? 'https' : 'http';
+	$url .= '://' . $_SERVER['SERVER_NAME'];
+	$url .= in_array( $_SERVER['SERVER_PORT'], array('80', '443') ) ? '' : ':' . $_SERVER['SERVER_PORT'];
+	$url .= $_SERVER['REQUEST_URI'];
+	
+	$stop_handle = apply_filters('v_forcelogin_stop_handle', false, $url);
+	if($stop_handle){
+		return false;
+	}
+	$whitelist = apply_filters('v_forcelogin_whitelist', array());
     $redirect_url = apply_filters('v_forcelogin_redirect', $url);
     if( preg_replace('/\?.*/', '', $url) != preg_replace('/\?.*/', '', wp_login_url()) && !in_array($url, $whitelist) ) {
-      wp_safe_redirect( wp_login_url( $redirect_url ), 302 ); exit();
+       wp_safe_redirect( wp_login_url( $redirect_url ), 302 ); exit();
     }
   }
 }
