df14637b7a0ba3482e379ed64b1b5b5610ab715a
Kevin Vess <kevin@vess.me>
2017-11-15 19:31:43 -0500

    Added license and cleaned up formatting

--- wp-force-login.php
+++ wp-force-login.php
@@ -10,23 +10,8 @@ Author URI: http://vess.me/
 Text Domain: wp-force-login
 Domain Path: /languages
 
-License: GPLv2 or later
-*/
-
-/*
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
 function v_forcelogin() {
@@ -48,11 +33,11 @@ function v_forcelogin() {
     $url .= $_SERVER['REQUEST_URI'];
 
     // Apply filters
-    $bypass = apply_filters ( 'v_forcelogin_bypass', false );
+    $bypass = apply_filters( 'v_forcelogin_bypass', false );
     $whitelist = apply_filters( 'v_forcelogin_whitelist', array() );
     $redirect_url = apply_filters( 'v_forcelogin_redirect', $url );
 
-    // Redirect visitors
+    // Redirect
     if ( preg_replace('/\?.*/', '', $url) != preg_replace('/\?.*/', '', wp_login_url()) && !in_array($url, $whitelist) && !$bypass ) {
       wp_safe_redirect( wp_login_url( $redirect_url ), 302 ); exit();
     }
@@ -66,17 +51,12 @@ function v_forcelogin() {
     }
   }
 }
-add_action('template_redirect', 'v_forcelogin');
+add_action( 'template_redirect', 'v_forcelogin' );
 
 function v_forcelogin_rest_access( $result ) {
   if ( !is_user_logged_in() ) {
-    return new WP_Error(
-      'rest_unauthorized',
-      __( 'Only authenticated users can access the REST API.', 'wp-force-login' ),
-      array( 'status' => rest_authorization_required_code() )
-    );
+    return new WP_Error( 'rest_unauthorized', __( "Only authenticated users can access the REST API.", 'wp-force-login' ), array( 'status' => rest_authorization_required_code() ) );
   }
-
   return $result;
 }
 add_filter( 'rest_authentication_errors', 'v_forcelogin_rest_access' );
